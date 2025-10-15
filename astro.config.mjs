80844a1ae7be80cf6c0db6ff6688491ecbdddcae
cris-01000011 <cris.01000011@gmail.com>
2025-10-14 19:45:42 -0600

    git pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cris-01000011.github.io/n64-games',
   vite: {
     plugins: [tailwindcss()]
   }
